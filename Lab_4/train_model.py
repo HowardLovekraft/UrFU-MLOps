582954571991cf204513bc667923960e68418d14
HowardLovekraft <nikolaiholkin2@gmail.com>
2026-04-05 20:58:33 +0500

    update train_model

--- Lab_4/train_model.py
+++ Lab_4/train_model.py
@@ -36,10 +36,10 @@ def train():
     X_train, X_val, y_train, y_val = train_test_split(X, Y,
                                                     test_size=0.3,
                                                     random_state=42)
+
+    with open('./signature.csv') as f:
+        f.write(X_train)
     
-    X_val.to_csv("./df_signature.csv", index=False)
- 
- 
     params = {'alpha': [0.0001, 0.001, 0.01, 0.05, 0.1 ],
             'l1_ratio': [0.001, 0.05, 0.01, 0.2],
             "penalty": ["l1","l2","elasticnet"],
